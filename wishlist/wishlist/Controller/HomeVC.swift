dca593af5eb8c9c68c5371e48bf2c9c57fcf54f3
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-22 18:34:08 -0500

    Monitor changes on textfields to enable/disable calculateBtn, display clearBtn when textfield is edited, add tap gesture to dismiss keyboard.

--- wishlist/wishlist/Controller/HomeVC.swift
+++ wishlist/wishlist/Controller/HomeVC.swift
@@ -9,12 +9,77 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    //MARK: IBOutlets
+    @IBOutlet weak var wageTextfield: UITextField!
+    @IBOutlet weak var priceTextfield: UITextField!
+    @IBOutlet weak var calculateBtn: RoundedButton!
+    @IBOutlet weak var hoursLabel: UILabel!
+    @IBOutlet weak var saveBtn: RoundedButton!
+    @IBOutlet weak var clearBtn: UIButton!
+    
+    //MARK: Variables
+    
+    //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //Monitor changes on textfields values
+        wageTextfield.addTarget(self, action: #selector(textfieldValueDidChange), for: .editingChanged)
+        priceTextfield.addTarget(self, action: #selector(textfieldValueDidChange), for: .editingChanged)
+        
+        //Disable btn when app starts
+        updateCalcBtn(isEnabled: false)
+        updateSaveBtn(isHidden: true)
+        updateClearBtnState(isHidden: true)
+        
+        //Tap gesture recognizer
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(viewTapped))
+        view.addGestureRecognizer(tapGesture)
     }
-
-
+    
+    func updateClearBtnState(isHidden: Bool) {
+        clearBtn.isHidden = isHidden
+        clearBtn.isEnabled = !isHidden
+    }
+    
+    func updateCalcBtn(isEnabled: Bool) {
+        if isEnabled {
+            calculateBtn.backgroundColor = UIColor(red: 0.059, green: 0.047, blue: 0.196, alpha: 1.0)
+            calculateBtn.isEnabled = true
+        } else {
+            calculateBtn.backgroundColor = UIColor.darkGray
+            calculateBtn.isEnabled = false
+        }
+    }
+    
+    func updateSaveBtn(isHidden: Bool) {
+        saveBtn.isHidden = isHidden
+        saveBtn.isEnabled = !isHidden
+    }
+    
+    @objc func textfieldValueDidChange() {
+        print("textfieldValueDidChange")
+        if wageTextfield.text != "" && priceTextfield.text != "" {
+            updateCalcBtn(isEnabled: true)
+        } else {
+            updateCalcBtn(isEnabled: false)
+        }
+        
+        updateClearBtnState(isHidden: false)
+    }
+    
+    @objc func viewTapped() {
+        view.endEditing(true)
+    }
+    
+    //MARK: IBActions
+    @IBAction func calculateBtnPressed(_ sender: Any) {
+        print("calculateBtnPressed")
+    }
+    
+    @IBAction func saveBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func clearBtnPressed(_ sender: Any) {
+    }
+    
 }
-
