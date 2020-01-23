f1ac45bb8f85a86541cf959bc900f9780b37d918
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-22 19:18:59 -0500

    fix statusbar color in NavigationController View.

--- wishlist/wishlist/View/NavigationController.swift
+++ wishlist/wishlist/View/NavigationController.swift
@@ -17,10 +17,6 @@ class NavigationController: UINavigationController {
     }
     
     override var preferredStatusBarStyle : UIStatusBarStyle {
-        if let topVC = viewControllers.last {
-            //return the status property of each VC, look at step 2
-            return topVC.preferredStatusBarStyle
-        }
         return .lightContent
     }
 
