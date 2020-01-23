4c2e5b36459b81b97692c7536adada34592acaa7
Kilyan SOCKALINGUM <sockalingum.kilyan@gmail.com>
2020-01-23 12:32:01 -0500

    Add function to fetch data from coreData, display custom cell fro each product.

--- wishlist/wishlist/Controller/MyProductsVC.swift
+++ wishlist/wishlist/Controller/MyProductsVC.swift
@@ -7,19 +7,41 @@
 //
 
 import UIKit
+import CoreData
+
 
 class MyProductsVC: UIViewController {
     //MARK: IBOutlets
     @IBOutlet weak var tableview: UITableView!
     
     //MARK: Variables
-    var products = [Product]()
+    var products: [Product] = []
     
     //MARK: Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableview.delegate = self
+        tableview.dataSource = self
+        tableview.isHidden = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchProductData()
+    }
+    
+    func fetchProductData() {
+        self.fetchData { (success) in
+            if success {
+                if self.products.count > 0 {
+                    self.tableview.isHidden = false
+                } else {
+                    self.tableview.isHidden = true
+                }
+            }
+        }
+        
+        self.tableview.reloadData()
     }
     
 }
@@ -35,7 +57,26 @@ extension MyProductsVC: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "productCell") as? ProductCell else { return UITableViewCell() }
-        
+        let product = products[indexPath.row]
+        cell.configureView(withName: product.name!, andWage: product.wage, forPrice: product.price, andResultHours: product.hours)
         return cell
+    }
+}
+
+//CoreData functions
+extension MyProductsVC {
+    
+    func fetchData(_ completion: @escaping CompletionHandler) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        let fetchRequest = NSFetchRequest<Product>(entityName: "Product")
+        
+        do {
+            products = try managedContext.fetch(fetchRequest)
+            print("Successfully fetch data")
+            completion(true)
+        } catch {
+            debugPrint(error.localizedDescription)
+            completion(false)
+        }
     }
 }
